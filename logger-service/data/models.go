0aa26ba3826061bc25091ccb12589b97a58b00dd
Andrew Limaru <andrew.limaru@gmail.com>
2022-08-29 16:14:35 +1000

    Update logger-service handler and fix logger-service model

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -16,7 +16,7 @@ type Models struct {
 }
 
 type LogEntry struct {
-	ID        string    `bson:"_id, omitempty" json:"id,omitempty"`
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
 	Name      string    `bson:"name" json:"name"`
 	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
